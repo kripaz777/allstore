2ea2be0658da61382de7edc3dbfd066ece5fdcbc
aiforcoral <aiforcoral@gmail.com>
2020-02-16 13:35:28 +0545

    update on requirements

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -136,4 +136,4 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 MEDIA_URL = 'images/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'')
-STATIC_ROOT = os.path.join(BASE_DIR, 'home/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '')
