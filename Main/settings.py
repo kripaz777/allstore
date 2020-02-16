eda507b73e7eb55a202a9b138f1d5050424467e8
aiforcoral <aiforcoral@gmail.com>
2020-02-17 01:25:45 +0545

    All update

--- Main/settings.py
+++ Main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^qb2stqu&7hs3)4%q*cme$1ppfvo=+=o94855w8y+ic7eq)+d+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['134.209.159.22']
 
