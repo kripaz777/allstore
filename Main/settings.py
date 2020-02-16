382d9b01a9668035b452f6f1908d0c4e092b2cf5
aiforcoral <aiforcoral@gmail.com>
2020-02-17 01:31:46 +0545

    All update

--- Main/settings.py
+++ Main/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = '/opt/myproject/images/'
 STATIC_URL = '/static/'
 #Redirection After Login Or Logout
 LOGIN_REDIRECT_URL = '/'
